c75bd5f400890130a7c49bf9bdf05f94ff32fe1f
Gabriel Freitas Dev <gabrielfreitas.ilustra@gmail.com>
2024-01-19 13:43:15 -0300

    ajustando conteudo dinamico da modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,25 +2,31 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 //pegar todos os botoes que existe com  a class check
 const checkButtons = document.querySelectorAll(".actions a.check")
 checkButtons.forEach(button => {
     //adicionar a escuta
-    button.addEventListener("click", event => {
-        //abrir modal
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
 
 const deleteButton = document.querySelectorAll(".actions a.delete")
 deleteButton.forEach(button => {
     //adicionar a escuta
-    button.addEventListener("click", event => {
-        //abrir modal
-        modal.open()
-    })
+    button.addEventListener("click", (event) => handleClick(event, false))
 })
 
-//pegar quando o marcar como lindo for clicado 
+function handleClick(event, check = true) {
+    event.preventDefault()
+    const text = check ? "Marcar como lida" : "Excluir"
 
-modal.open()
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
+    check? modalButton.classList.remove("red") : modalButton.classList.add("red")
+    //abrir modal
+    modal.open()
+}
